2407016ca63f6cf1d03a8d7ba2408a7ca14e4959
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-07-29 17:50:38 -0300

    Add ExtractStringsToConstantsCommand to create constants for each selected string word.

--- javascript_util.py
+++ javascript_util.py
@@ -1,1 +1,36 @@
 import sublime, sublime_plugin, string_util as su
+
+def getVarName(string):
+	return su.uncamelize(string, "_").upper()
+
+def getVarValue(string):
+	return su.decapitalize(su.camelize(string))
+
+class ExtractStringsToConstantsCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+    	output = ""
+    	wordList = []
+
+    	pattern = re.compile("['\"][a-zA-Z]+['\"]")
+
+    	for region in self.view.sel():
+    		selectedContent = self.view.substr(region)
+
+    		for rLine in self.view.lines(region):
+    			for word in pattern.findall(self.view.substr(rLine)):
+    				strippedWord = re.sub("['\"]", "", word)
+    				wordList.append(strippedWord);
+    				selectedContent = selectedContent.replace(word, getVarName(strippedWord))
+
+    		self.view.replace(edit, region, selectedContent)
+
+      	wordList.sort(su.strcmp)
+      	wordList = list(set(wordList))
+
+    	for word in wordList:
+    		name = getVarName(word)
+    		value = getVarValue(word)
+    		output += name + " = '" + value + "',\n"
+
+		sublime.set_clipboard(output)
+		sublime.status_message("Copied extracted words to clipboard.")
--- string_util.py
+++ string_util.py
@@ -1,12 +1,7 @@
 import sublime, sublime_plugin, re
 
-def uncamelize(stringAsCamelCase, separator = "-"):
-	if stringAsCamelCase is None:
-		return None
-
-	pattern = re.compile('([A-Z][A-Z][a-z])|([a-z][A-Z])')
-
-	return pattern.sub(lambda m: m.group()[:1] + separator + m.group()[1:], stringAsCamelCase).lower()
+def strcmp(a, b):
+	return cmp(a.lower(), b.lower())
 
 def camelize(string, separator = "-_ "):
 	if string is None:
@@ -16,6 +11,16 @@ def camelize(string, separator = "-_ "):
 
 	return pattern.sub(lambda m: m.group()[1:].upper(), string)
 
+def decapitalize(string):
+	return string[0:1].lower() + string[1:]
+
+def uncamelize(stringAsCamelCase, separator = "-"):
+	if stringAsCamelCase is None:
+		return None
+
+	pattern = re.compile('([A-Z][A-Z][a-z])|([a-z][A-Z])')
+
+	return pattern.sub(lambda m: m.group()[:1] + separator + m.group()[1:], stringAsCamelCase).lower()
 
 class CamelizeTextCommand(sublime_plugin.TextCommand):
     def run(self, edit):
